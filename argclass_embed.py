2b8d12e6abad08c6961e5ac2286d9d3237a5068c
jasons1425 <jasonns1425@gmail.com>
2022-03-18 01:19:14 +0800

    Update result summary

--- argclass_embed.py
+++ argclass_embed.py
@@ -15,19 +15,19 @@ df_train, df_valid = load_data_as_df("train"), load_data_as_df("valid")
 # valid_loader_cls = get_loader(valid_cls.sequence, valid_cls.target - 1,
 #                               pad_len=PAD_LEN, batch_size=BATCH_SIZE, label_enc=True, add_sampler=False)
 train_loader_cls = get_loader(df_train.sequence, df_train.target,
-                              pad_len=PAD_LEN, batch_size=BATCH_SIZE, label_enc=True, add_sampler=True)
+                              pad_len=PAD_LEN, batch_size=BATCH_SIZE, label_enc=True, add_sampler=False)
 valid_loader_cls = get_loader(df_valid.sequence, df_valid.target,
-                              pad_len=PAD_LEN, batch_size=BATCH_SIZE, label_enc=True, add_sampler=True)
+                              pad_len=PAD_LEN, batch_size=BATCH_SIZE, label_enc=True, add_sampler=False)
 dataloaders = {"train": train_loader_cls, "valid": valid_loader_cls}
 
 
 # setting up objects for training
 ENC_DIM = 512
 IN_DIM, OUT_DIM = 23+1, 15
-IN_KSIZE, RES_KSIZE = 15, 3
-RES_DIM, RES_BLKSIZE, RES_DIL = 256, 2, 2
-FC_BLKS = [8448, 1000]
-ACT, DROPOUT = torch.nn.ReLU, 0.5
+IN_KSIZE, RES_KSIZE = 5, 3
+RES_DIM, RES_BLKSIZE, RES_DIL = 128, 2, 2
+FC_BLKS = [4224, 1000]
+ACT, DROPOUT = torch.nn.ReLU, 0.6
 LR, MOMENTUM, DECAY = 1e-3, 0.9, 0.01
 HALF = True
 device = "cuda" if torch.cuda.is_available() else "cpu"
